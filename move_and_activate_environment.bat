549af88bf518baf4c10bbd952b608b619f158d80
cnlutz20 <cnlutz20@gmail.com>
2024-11-18 10:57:48 -0500

    adding batch file to activate environment from project files

---
+++ move_and_activate_environment.bat
@@ -0,0 +1,10 @@
+@echo off
+REM Navigate to the Scripts folder
+cd C:\Users\clutz\hunt_env\Scripts
+REM Run the Activate.ps1 script
+call activate.bat
+echo Ran Activation file
+REM Return to the original folder
+cd C:\Users\clutz\Projects
+echo Returned to C:\Users\clutz\Projects
+pause
